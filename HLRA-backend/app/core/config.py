b486fee4f2839b21175dc82c9b60940c7ffcbabb
nikkvijay <jatalenikhileshwar@gmail.com>
2025-07-05 02:14:39 +0530

    fix:added new origin

--- HLRA-backend/app/core/config.py
+++ HLRA-backend/app/core/config.py
@@ -38,7 +38,8 @@ class Settings(BaseSettings):
             "http://localhost:3000",
             "http://127.0.0.1:3000", 
             "http://localhost:5173",
-            "http://127.0.0.1:5173"
+            "http://127.0.0.1:5173",
+            "https://health-lab-report-analyzer.vercel.app"
         ]
     
     @property
